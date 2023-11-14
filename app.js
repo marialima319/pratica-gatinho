3dba1d9fdaddea6bd51e599ca543030650b2093c
marialima319 <131209980+marialima319@users.noreply.github.com>
2023-11-13 21:47:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Gatinhos
 const buscarGatinhos = (e) => {
     e.preventDefault()
     const xhr = new XMLHttpRequest()
@@ -10,15 +11,43 @@ const buscarGatinhos = (e) => {
                     const img = document.createElement('img')
                     img.src = cat.url
                     document.querySelector("#gatinhos").appendChild(img)
-                    
+                
                 });
             } else {
                 alert('Erro na requisição')
             }
         }
-
     }
     xhr.send()
 }
 const btnMostrar = document.querySelector("#mostrar-gatinhos")
 btnMostrar.addEventListener("click", buscarGatinhos)
+
+
+//Marcas de Carros
+
+const getMarcas = () => {
+    const tarefas = fetch('https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json')
+
+
+    tarefas
+        .then(resposta => resposta.json())
+        .then(marcas => {
+            const ul = document.createElement('ul')
+            marcas.forEach(marca => {
+                const li = document.createElement('li')
+                const logo = document.createElement('img')
+                logo.src = marca.image?.optimized
+                li.appendChild(logo)
+                ul.appendChild(li)
+                console.log(marca)
+
+            })
+            document.body.appendChild(ul)
+
+        })
+        .catch(erro => console.log(erro))
+                  
+}
+const btnMarcas = document.querySelector("#marcas")
+btnMarcas.addEventListener("click", getMarcas)
